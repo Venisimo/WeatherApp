e9371c26b33fec222d6557bf985e795100304a0b
Venisimo <ledokol522@gmail.com>
2023-05-10 23:10:29 +0700

    server on localhost and new api

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-import express from 'express';
-import nunjucks from 'nunjucks';
-import __dirname from './__dirname.js';
-
-let days = {20: {morning: '-20', day: '-8', evening: '-10', night: '-12'}, 
-            21: {morning: '-12', day: '-5', evening: '-9', night: '-14'},
-            22: {morning: '-10', day: '-2', evening: '-4', night: '-7'},
-            23: {morning: '-5', day: '+1', evening: '+2', night: '-1'}, 
-            24: {morning: '-2', day: '+3', evening: '+2', night: '-1'}, 
-            25: {morning: '-2', day: '+4', evening: '+5', night: '+3'}, 
-            26: {morning: '+2', day: '+4', evening: '+3', night: '+2'}
-            };
-let app = express();
-
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app    
-});
-
-app.get('/', function(req, res){
-    res.render('index.html', {"days": days})
-});
-
+import playwright from 'playwright';
+async function func() {
+    const browser = await playwright['chromium'].launch({ headless: false,});
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://www.gismeteo.ru/weather-novosibirsk-4690/tomorrow/");
     
+    let articles =  await page.$$("article");
 
-app.listen(3000, function(req, res) {
-	console.log('running');
-})
+    let data = [];                 
+    for(let article of articles)
+    {
+        data.push({
+            temperature: await article.$eval("span.unit unit_temperature_c", el => el.textContent),
+        });
+    } 
+    console.log(data);
+}    
+func();  
