f1db19d74859747574b1cb65d50361e777c62fd3
Venisimo <ledokol522@gmail.com>
2023-10-01 23:23:29 +0700

    changed background, add 404 page and changed not found location

--- ServerAndRoutes.js
+++ ServerAndRoutes.js
@@ -5,25 +5,25 @@ import path from 'path';
 let app = express();
 
 app.get('/weatherNow', async function(req, res){
-    res.sendFile(__dirname + '/client/pages/weatherNow.html');
+    res.sendFile(__dirname + '/ClientAndServer/pages/weatherNow.html');
 });
 
 app.get('/weatherThreeDays', function(req, res){
-    res.sendFile(__dirname + '/client/pages/weatherThreeDays.html');
+    res.sendFile(__dirname + '/ClientAndServer/pages/weatherThreeDays.html');
 });
 
 app.get('/weatherWeek', function(req, res){
-    res.sendFile(__dirname + '/client/pages/weatherWeek.html');
+    res.sendFile(__dirname + '/ClientAndServer/pages/weatherWeek.html');
 });
 
 app.get('/weatherTenDays', function(req, res){
-    res.sendFile(__dirname + '/client/pages/weatherTenDays.html');
+    res.sendFile(__dirname + '/ClientAndServer/pages/weatherTenDays.html');
 });
 
-app.use(express.static(path.join(__dirname, '/client/')))
+app.use(express.static(path.join(__dirname, '/ClientAndServer/')))
     
 app.use(function(req, res, next) {
-    res.status(404).sendFile('/Лысов_НАДИП/4 курс/Project_Nadip/ClientAndServer/pages/Page404.html');
+    res.status(404).sendFile(path.join(__dirname,'/ClientAndServer/pages/Page404.html'));
 });
 
 app.listen(3001, function(req, res) {
